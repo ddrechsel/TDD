7819913f3d157ba600826fc0b70e6c3692af4af5
ddrechsel <dan@drechsel.us>
2015-01-19 09:26:03 -0500

    Functional test checks we can input To-do list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 import unittest
 
@@ -16,18 +18,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # Make sure home page loads and is correct one
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+		
 # A user can enter a To-do items
-
+        self.assertEqual(
+            inputbox.get.attribute('placeholder'),
+                'Enter a to-do item'
+        )
+            
 # Let her enter "Buy peacock feathers" into a text box
-
+        inputbox.send_keys('Buy peacock feathers')
+	
 # Upon entry, page updates and the page lists:
 # 1: Buy peacock feathers" as an item in the to-do list
-
+        inputbox.send_keys(Keys.ENTER)
+	
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+	
 # There is another text box awaiting further items
 # She enters "Use peacock feathers to make a fly"
-
+        self.fail('Finish the Test!')
+	
 # page updates again
 
 # site generates unique url for saved to-do list
