fdc9e2c52c8290ac05a85513a702b62ffb573c17
ddrechsel <dan@drechsel.us>
2015-01-20 07:26:35 -0500

    Gathering data entry

--- functional_tests.py
+++ functional_tests.py
@@ -22,8 +22,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 		
 # A user can enter a To-do items
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get.attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
             
